00251cc4decb02a2e405286916db3ed2d59c69f3
colin-h <colinharman@utexas.edu>
2015-06-11 10:26:52 -0500

    started stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.counter = 0;
+
+  return someInstance;
+};
+// how are they different
+
+var stackMethods = {
+  size: function(){
+    return this.counter;
+  },
+
+  push: function(value){
+    this.someInstance[this.counter] = value;
+    this.counter++;
+
+  }
 };
 
-var stackMethods = {};
+// how are these Stack's the same?
 
 
