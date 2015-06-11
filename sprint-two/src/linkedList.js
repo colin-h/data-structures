a03a8422462e7f1b153da9e2ded90c797bab495e
colin-h <colinharman@utexas.edu>
2015-06-11 15:20:32 -0500

    work on linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,9 +4,27 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if(list.tail === null){
+      list.tail = Node(value);
+      list.head = Node(value);
+      // console.log(list);
+    }
+    else{
+      list.tail = Node(value);
+      // console.log(list.tail);
+    }
+
   };
 
   list.removeHead = function(){
+
+    var result = list.head.value;
+
+    // list.head = list.head[next];
+    console.log(list.head);
+
+
+    return result;
   };
 
   list.contains = function(target){
