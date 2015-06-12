784304c2ccc353c50910fbfa4aa22557cbf83765
MKS-students <mks.students@hackreactor.com>
2015-06-11 20:22:27 -0500

    almost completed tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,13 @@
 var Tree = function(value){
+
   var newTree = {};
+
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+
+  _.extend(newTree, treeMethods)
 
   return newTree;
 };
@@ -16,9 +20,36 @@ var treeMethods = {};
 
 treeMethods.addChild = function(value){
 
+  var treeChild = Tree(value);
+
+  this.children.push(treeChild);
+
 };
 
 treeMethods.contains = function(target){
+  var test = this;
+  console.log(test);
+
+
+  var recurse = function(newTree){
+
+    if (newTree.value===target) {
+      return true;
+
+    } else if (newTree.children===[]) {
+        // debugger;
+        return false;
+      }
+    else {
+      for (var i=0; i<newTree.children.length;i++) {
+        return recurse(newTree.children[i]);
+      };
+      // debugger;
+      return false;
+    }
+  };
+
+  return recurse(this);
 
 };
 
