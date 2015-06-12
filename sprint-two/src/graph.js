95c2e2e1b610853cce3d25bc71fdec5817b8e13a
MKS-students <mks.students@hackreactor.com>
2015-06-12 16:47:49 -0500

    graph almost done

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -3,54 +3,63 @@
 var Graph = function(){
 
   this.storage = {}
-  this.nodeCounter = 0
 
 
 };
 
-var Planet = function(value) {
-  this.edgeCounter = 0;
-  this.value = value;
-};
 
-// planet 1 is a string i.e. the value of a planet
-var Edge = function(planet1,planet2) {
+Graph.prototype.addNode = function(node){
 
-  this.planet1 = planet1;
-  this.planet2 = planet2;
+  this.storage[node] = [];
 
-};
-
-
-Graph.prototype.addNode = function(value){
-  var newPlanet = new Planet(value);
-
-
-  this.storage[this.nodeCounter] = newPlanet;
-  this.nodeCounter++;
 };
 
 Graph.prototype.contains = function(value){
 
-  for (var i = 0; i < this.nodeCounter; i++){
-    if (this.storage[i].value === value) {
-      return true;
-    };
-  };
+  if (this.storage[value] === undefined) {
+    return false
+  }
 
-  return false;
+  return true
 };
 
 Graph.prototype.removeNode = function(node){
+
+
+  for (var i = 0; i < this.storage[node].length; i++){
+    for (var j = 0; j < this.storage[node][i].length; j++){
+      if (this.storage[node][i][j] === node){
+        this.storage[node][i].splice(j, 1);
+      }
+    }
+  }
+
+  delete this.storage[node];
+
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+
+  for (var i =0; i < this.storage[fromNode].length; i++){
+    if (this.storage[fromNode][i] === toNode){
+      return true;
+    }
+  }
+  return false;
+
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+
+  this.storage[fromNode].push(toNode);
+  this.storage[toNode].push(fromNode);
+
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+
+    var place = this.storage[fromNode].indexOf(toNode);
+    this.storage[fromNode].splice(place, 1)
 };
 
 Graph.prototype.forEachNode = function(cb){
