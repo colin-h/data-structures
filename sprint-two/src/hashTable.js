34ba72acf4acb855ea2f1483925a6cf2353f2acc
MKS-students <mks.students@hackreactor.com>
2015-06-12 20:12:50 -0500

    hashtable done

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,18 +1,47 @@
 var HashTable = function(){
+
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  for (var i = 0; i<this._limit; i++) {
+    this._storage.set(i, [])
+  }
+
 };
 
 HashTable.prototype.insert = function(k, v){
+  // debugger;
   var i = getIndexBelowMaxForKey(k, this._limit);
+  //var currentBucket=this._storage
+  var currentBucket = this._storage.get(i);
+  currentBucket[k]=v;
+  //this._storage.set(currentBucket,i)
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var currentBucket = this._storage.get(i);
+  // debugger;
+  if (!currentBucket[k]){
+    return null;
+  }
+  else {
+    return currentBucket[k];
+  }
+
 
 };
 
 HashTable.prototype.remove = function(k){
+
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var currentBucket = this._storage.get(i);
+
+  if (!currentBucket[k]){
+    return null;
+  }
+  else {
+    delete currentBucket[k];
+  }
 
 };
 
